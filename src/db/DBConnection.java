26e9dd30e3966c03eaefcb7d870153420415e391
limxinli <www.limxinli@hotmail.com>
2017-06-08 17:28:15 +0800

    Database connection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
 
 	public static Connection getConnection() throws Exception {
 		Class.forName("com.mysql.jdbc.Driver");
-		String connURL = "jdbc:mysql://127.0.0.1/sectestdb?user=sectest&password=1qwer$#@!";
+		String connURL = "jdbc:mysql://localhost/sectestdb?user=sectest&password=1qwer$#@!";
 		Connection conn = DriverManager.getConnection(connURL);
 
 		return conn;
